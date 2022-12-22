19afb8ed534cce93ce97d03de813b288cdc9f539
Uttam Bhagaur <uttambhagaur@gmail.com>
2022-12-22 22:15:21 +0530

    basic structure ready for app

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HeroService } from '../hero.service';
+import { MessageService } from '../message.service';
+import { HEROES } from '../mock-heroes';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  selectedHero?: Hero;
+
+  heroes:Hero[] = [];
+
+  constructor(private heroService: HeroService, private messageService: MessageService){}
+  ngOnInit(): void {
+    this.getHeroes();
+  }
+
+  onSelectHero(hero:Hero){
+    this.messageService.add(`HeroesComponent: Selected hero id=${hero.id}`);
+    this.selectedHero = hero;
+  }
+
+  getHeroes(): void{
+     this.heroService.getHeroes().subscribe(heroes => this.heroes = heroes);
+  }
+
+}
---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,10 @@
+import { Hero } from "./hero";
+
+export const HEROES: Hero[] = [
+    {id:12, name:'Dr. Nice'},
+    {id:13, name:'Bombasto'},
+    {id:14, name:'Celeritas'},
+    {id:15, name:'Magneta'},
+    {id:16, name:'RubberMan'},
+    {id:17, name:'IceMan'},
+];
